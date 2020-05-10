6aae9408c68643ceae29c80cb526b000194be490
Naoaki Obiki <obiki.wk@gmail.com>
2020-05-10 20:58:30 +0900

    :sparkles: new vim

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+cd $(dirname `realpath $0`)
+
+# set environments
+cat environments >> ~/.bash_profile
+
+# create symlink .vim
+ln -s ${PWD}/.vim ${HOME}/.vim
+ln -s ${PWD}/.vimrc ${HOME}/.vimrc
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim -c "PlugInstall"
+
+
