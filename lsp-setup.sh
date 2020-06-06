070481f4a718c8ae57fbd44a192cb8cdcfcf9c8d
Naoaki Obiki <obiki.wk@gmail.com>
2020-06-06 21:03:03 +0900

    lsp-setup php

--- lsp-setup.sh
+++ lsp-setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -eu
 cd $(dirname `realpath $0`)
 
+NODENV_VERSION=14.3.0
+
 if [ "${1}" == "python" ]; then
     sudo apt update
     sudo apt install -y build-essential python3-venv python3-dev
     vim -c "set ft=python" -c "LspInstallServer"
 elif [ "${1}" == "php" ]; then
-    echo "php"
+    nodenv install $NODENV_VERSION
+    nodenv global $NODENV_VERSION
+    vim -c "set ft=php" -c "LspInstallServer"
 else
     echo "Usage: lsp-setup.sh {python,php}"
 fi
