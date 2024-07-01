76302d833132873af2c8d42f642a88cee0616635
Naoaki Obiki <obiki.wk@gmail.com>
2024-07-01 11:40:04 +0900

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ cat environments >> ~/.bash_profile
 # create symlink .vim
 ln -s ${PWD}/.vim ${HOME}/.vim
 ln -s ${PWD}/.vimrc ${HOME}/.vimrc
-ln -s ${PWD}/.editorconfig ${HOME}/.editorconfig
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
