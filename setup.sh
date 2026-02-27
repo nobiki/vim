6fba4320c9cafa0a900f923bb26af9cd841260d3
Naoaki Obiki <obiki.wk@gmail.com>
2026-02-27 11:04:57 +0900

    nvim

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ cd $(dirname `realpath $0`)
 cat environments >> ~/.bash_profile
 
 # create symlink .vim
-ln -s ${PWD}/.vim ${HOME}/.vim
-ln -s ${PWD}/.vimrc ${HOME}/.vimrc
+ln -sf ${PWD}/.vim ${HOME}/.vim
+ln -sf ${PWD}/.vimrc ${HOME}/.vimrc
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
